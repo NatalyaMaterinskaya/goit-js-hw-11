f7a27d1e0dc8aec80c5cf756d3d840f90ee5d508
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-07-17 04:08:32 +0300

    Added scroll

--- src/img-api.js
+++ src/img-api.js
@@ -3,19 +3,23 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38292054-24d10c6ab751dde75bf1a43b0';
 
-
-export async function getImg(param) {
-    const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=yellow+flowers&image_type=photo`,
-      {
-        params: {
-          q: param,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-        },
-      }
-    );
-    const data=response.data.hits;
-    return data;
+export async function getImg(param, page) {
+  const response = await axios.get(
+    `${BASE_URL}?key=${API_KEY}`,
+    {
+      params: {
+        q: param,
+        per_page: 40,
+        page:page,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    }
+  );
+  const data = {
+    totalHits: response.data.totalHits,
+    hits: response.data.hits,
+  };
+  return data;
 }
--- src/index.js
+++ src/index.js
@@ -7,40 +7,95 @@ const formEl = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
 const targetEl = document.querySelector('.guard');
 
+let lightbox = new SimpleLightbox('.photo-card a');
+let param;
 let currentPage = 1;
 let options = {
-    root: null,
-    rootMargin: '300px',
-    threshold: 1.0
-}
+  root: null,
+  rootMargin: "300px",
+  threshold: 1.0,
+};
 
-var observer = new IntersectionObserver(onLoad, options);
+let observer = new IntersectionObserver(onLoad, options);
 
 function onLoad(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        currentPage += 1;
-      }
-    });
-}
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      console.log(param);
+      getImg(param, currentPage)
+        .then(data => {
+          if (data.hits.length === 0) {
+            throw new Error();
+          }
+          const markup = data.hits
+            .map(
+              ({
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+              }) =>
+                `<div class="photo-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" width="300" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              <span>${likes}</span>
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              <span>${views}</span>
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              <span>${comments}</span>
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              <span>${downloads}</span>
+            </p>
+          </div>
+        </a>
+    </div>`
+            )
+            .join('');
 
-formEl.addEventListener('submit', searchImg);
+          galleryEl.insertAdjacentHTML('beforeend', markup);
+          lightbox.refresh();
+          console.log(data.totalHits / 40);
+          // if (currentPage >= data.totalHits / 40) {
+          //   observer.unobserve(targetEl);
+          // }
+        })
+        .catch(error =>
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          )
+        );
+    }
+  });
+}
 
 function searchImg(event) {
   event.preventDefault();
-  galleryEl.innerHTML='';
+  galleryEl.innerHTML = '';
   const {
-    elements: {searchQuery},
+    elements: { searchQuery },
   } = event.currentTarget;
-  
-  const param = searchQuery.value;
 
-  getImg(param)
+  param = searchQuery.value;
+
+  getImg(param, currentPage)
     .then(data => {
-      if (data.length === 0) {
+      if (data.hits.length === 0) {
         throw new Error();
       }
-      const markup = data
+      const markup = data.hits
         .map(
           ({
             webformatURL,
@@ -77,9 +132,9 @@ function searchImg(event) {
         )
         .join('');
 
-      galleryEl.insertAdjacentHTML('afterbegin', markup);
-
-      let lightbox = new SimpleLightbox('.photo-card a');
+      galleryEl.insertAdjacentHTML('beforeend', markup);
+      observer.observe(targetEl);
+      lightbox.refresh();
     })
     .catch(error =>
       Notiflix.Notify.failure(
@@ -87,3 +142,5 @@ function searchImg(event) {
       )
     );
 }
+
+formEl.addEventListener('submit', searchImg);
