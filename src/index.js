1c47da03d5f16b01e863bd6584ccb4f8c8c5381a
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-07-17 11:35:20 +0300

    Finished project

--- src/index.js
+++ src/index.js
@@ -12,77 +12,16 @@ let param;
 let currentPage = 1;
 let options = {
   root: null,
-  rootMargin: "300px",
+  rootMargin: '300px',
   threshold: 1.0,
 };
 
-let observer = new IntersectionObserver(onLoad, options);
-
-function onLoad(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      currentPage += 1;
-      console.log(param);
-      getImg(param, currentPage)
-        .then(data => {
-          if (data.hits.length === 0) {
-            throw new Error();
-          }
-          const markup = data.hits
-            .map(
-              ({
-                webformatURL,
-                largeImageURL,
-                tags,
-                likes,
-                views,
-                comments,
-                downloads,
-              }) =>
-                `<div class="photo-card">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" width="300" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              <span>${likes}</span>
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              <span>${views}</span>
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              <span>${comments}</span>
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              <span>${downloads}</span>
-            </p>
-          </div>
-        </a>
-    </div>`
-            )
-            .join('');
-
-          galleryEl.insertAdjacentHTML('beforeend', markup);
-          lightbox.refresh();
-          console.log(data.totalHits / 40);
-          // if (currentPage >= data.totalHits / 40) {
-          //   observer.unobserve(targetEl);
-          // }
-        })
-        .catch(error =>
-          Notiflix.Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          )
-        );
-    }
-  });
-}
+formEl.addEventListener('submit', searchImg);
 
 function searchImg(event) {
   event.preventDefault();
+  observer.unobserve(targetEl);
+  currentPage = 1;
   galleryEl.innerHTML = '';
   const {
     elements: { searchQuery },
@@ -143,4 +82,69 @@ function searchImg(event) {
     );
 }
 
-formEl.addEventListener('submit', searchImg);
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      getImg(param, currentPage)
+        
+        .then(data => {
+          if (data.hits.length === 0) {
+            throw new Error();
+          }
+          const markup = data.hits
+            .map(
+              ({
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+              }) =>
+                `<div class="photo-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" width="300" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              <span>${likes}</span>
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              <span>${views}</span>
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              <span>${comments}</span>
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              <span>${downloads}</span>
+            </p>
+          </div>
+        </a>
+    </div>`
+            )
+            .join('');
+
+          galleryEl.insertAdjacentHTML('beforeend', markup);
+          lightbox.refresh();
+          if (currentPage >= data.totalHits / 40) {
+            observer.unobserve(targetEl);
+            Notiflix.Notify.info(
+              "We're sorry, but you've reached the end of search results."
+            );
+          }
+        })
+        .catch(error =>
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          )
+        );
+    }
+  });
+}
